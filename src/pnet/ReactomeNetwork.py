d1a4bc4f5be7d66c6e7f90b3e26c4501d1b29290
FilippoGas <filippo.gastaldell@gmail.com>
2025-12-01 16:33:01 +0100

    updated matrix indexing syntax

--- src/pnet/ReactomeNetwork.py
+++ src/pnet/ReactomeNetwork.py
@@ -201,13 +201,14 @@ class ReactomeNetwork:
                         # Add connections only if there are sufficient inflows
                         # Add genes to pathways connections to adjacency of layer
                         genes_in_pathway = self.pathway2genes[self.pathway2genes['pathway'] == pathway]['gene']
-                        gene_connections[pathway][genes_in_pathway] = 1
-
+                        #gene_connections[pathway][genes_in_pathway] = 1
+                        gene_connections.loc[genes_in_pathway, pathway] = 1
                         # Add pathway to pathways connections to adjacency of layer
                         pathways_in_pathway = [n[0] for n in self.graph.in_edges(pathway)]
                         pathways_in_pathway = list(set(pathways_in_pathway).intersection(higher_level_pathway_nodes))
                         for p in pathways_in_pathway:
-                            pathway_connections[p][pathway] = 1
+                            #pathway_connections[p][pathway] = 1
+                            pathway_connections.loc[pathway, p] = 1
 
                 gene_layers.append(gene_connections)
                 pathway_layers.append(pathway_connections)
