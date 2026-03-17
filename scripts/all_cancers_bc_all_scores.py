2b919b7aa9b7bb944cfab01cd743e479a9724be3
FilippoGas <filippo.gastaldell@gmail.com>
2026-03-17 11:02:35 +0100

    separated PLL and PLLR to reduce memory usage during aggregation or result dataframes

--- scripts/all_cancers_bc_all_scores.py
+++ scripts/all_cancers_bc_all_scores.py
@@ -13,20 +13,20 @@ from util import util, sankey_diag
 # Read params
 tumor_type  = sys.argv[1]
 gene_list   = sys.argv[2]
+score_type  = sys.argv[3]
 
 # Paths
 input_dir           = "/shares/CIBIO-Storage/BCG/scratch/fgastaldello/data/pnet_fork/pnet_gene/all_cancers/aggregated_scores"
 cancer_genes_list   = "/shares/CIBIO-Storage/BCG/scratch/fgastaldello/data/pnet_fork/resources/gene_lists/CancerGenesList.csv"
-output_dir          = "/shares/CIBIO-Storage/BCG/scratch/fgastaldello/data/pnet_fork/pnet_gene/all_cancers/bc/"+gene_list+"_all_scores/"+tumor_type+"/plots/"
+output_dir          = "/shares/CIBIO-Storage/BCG/scratch/fgastaldello/data/pnet_fork/pnet_gene/all_cancers/bc/"+gene_list+"_all_scores/"+tumor_type+"/plots"
 tumor_types_path    = "/shares/CIBIO-Storage/BCG/scratch/fgastaldello/data/pnet_fork/resources/sample_cancer_type/all_cancers/cancer_type_"+tumor_type+".csv"
 
 # Load ALL scores and sample data
 genetic_data = dict()
 for agg_func in ["avg", "sd", "max", "min", "delta"]:
-    for score_type in ["PLL", "PLLR"]:
-        scores_hap1, scores_hap2 = util.load_hap_scores(input_dir+"/"+score_type+"/"+agg_func+"/", agg_func, score_type)
-        genetic_data[agg_func+"_"+score_type+"_hap1"] = scores_hap1
-        genetic_data[agg_func+"_"+score_type+"_hap2"] = scores_hap2
+    scores_hap1, scores_hap2 = util.load_hap_scores(input_dir+"/"+score_type+"/"+agg_func+"/", agg_func, score_type)
+    genetic_data[agg_func+"_"+score_type+"_hap1"] = scores_hap1
+    genetic_data[agg_func+"_"+score_type+"_hap2"] = scores_hap2
 
 # Load tumor types
 tumor_types =  pd.read_csv(tumor_types_path, sep=",").dropna().set_index("sample")
@@ -60,7 +60,7 @@ for fold, (train_index, test_index) in enumerate(kf.split(samples, tumor_types[t
         batch_size=128, epochs=3000, early_stopping=True, train_inds=train_sample,
         test_inds=test_sample, input_dropout=0.5, gene_set=selected_genes
     )
-
+    print(f'done run for fold {fold}, starting interpretation')
     # Perform evaluation, generate plots and importances csv and save
     plt.clf()
     results = Pnet.evaluate_and_interpret(
@@ -68,7 +68,7 @@ for fold, (train_index, test_index) in enumerate(kf.split(samples, tumor_types[t
         test_dataset,
         tumor_types.columns.values
     )
-
+    print(f'Done interpretation for fold {fold}')
     all_gene_feature_importances.append(results['gene_feature_importances'])
     all_additional_feature_importances.append(results['additional_feature_importances'])
     all_gene_importances.append(results['gene_importances'])
